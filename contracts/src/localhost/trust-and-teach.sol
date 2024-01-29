c5c30bf9db429fe83f5db88bbb084da4a40b1834
Kirill Igumenshchev <kirill.igum@gmail.com>
2024-01-29 14:01:31 -0500

    Added llmSteps parameter to sendInstructionPrompt and cartesiSubmitPrompt functions, and updated submitPrompt function to accept llmSteps parameter.

--- contracts/src/localhost/trust-and-teach.sol
+++ contracts/src/localhost/trust-and-teach.sol
@@ -20,6 +20,7 @@ contract TrustAndTeach {
     struct Conversation {
         address author;
         string prompt;
+        uint256 llmSteps;
         string[][] responses;
         uint256 rankSubmissionCount;
         mapping(address => RankSubmission) rankSubmissions;
@@ -40,23 +41,28 @@ contract TrustAndTeach {
         L2_DAPP = l2_dapp;
     }
 
-    function sendInstructionPrompt(string memory prompt) public {
+    function sendInstructionPrompt(uint256 llmSteps, string memory prompt)
+        public
+    {
         // require(L2_DAPP != address(0));
         Conversation storage conversation = conversations[
             current_conversation_id
         ];
         conversation.author = msg.sender;
         conversation.prompt = prompt;
+        conversation.llmSteps = llmSteps;
         conversation.createInstructionTimestamp = block.timestamp;
-        cartesiSubmitPrompt(current_conversation_id, prompt);
-        emit PromptSent(current_conversation_id, prompt);
+        cartesiSubmitPrompt(current_conversation_id, llmSteps, prompt);
+        emit PromptSent(current_conversation_id, llmSteps, prompt);
         current_conversation_id++;
     }
 
-    function cartesiSubmitPrompt(uint256 conversation_id, string memory prompt)
-        public
-    {
-        bytes memory payload = abi.encode(conversation_id, prompt);
+    function cartesiSubmitPrompt(
+        uint256 conversation_id,
+        uint256 llmSteps,
+        string memory prompt
+    ) public {
+        bytes memory payload = abi.encode(conversation_id, llmSteps, prompt);
         inputBox.addInput(L2_DAPP, payload);
     }
 
